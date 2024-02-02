05f650f219acc8d32990f1823a0040a4e205db30
Mauri Toribio <mauri.toribio@entrust.com>
2024-02-02 16:11:46 +0100

    cobra testing

---
+++ cmd/root.go
@@ -0,0 +1,35 @@
+/*
+Copyright © 2023 Matt J. Wiater matt.wiater@gmail.com
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var Verbose bool
+var Debug bool
+
+var rootCmd = &cobra.Command{
+	Use:   "directorylist",
+	Short: "List the size of a local directory.",
+	Long:  `This command will display the size of a directory with several different options.`,
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	rootCmd.PersistentFlags().BoolVarP(&Verbose, "verbose", "v", false, "Display more verbose output in console output. (default: false)")
+	viper.BindPFlag("verbose", rootCmd.PersistentFlags().Lookup("verbose"))
+
+	rootCmd.PersistentFlags().BoolVarP(&Debug, "debug", "d", false, "Display debugging output in the console. (default: false)")
+	viper.BindPFlag("debug", rootCmd.PersistentFlags().Lookup("debug"))
+}
--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"github.com/maurotory/jenkins-go-cli/cmd"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	cmd.Execute()
 
 }
